a025b92ee5e44378cf13b198eb34a72ddc6271d5
Rob Ward <wardrobert@gmail.com>
2020-12-04 23:04:26 -0500

    update for three numbers

--- day1/report.py
+++ day1/report.py
@@ -3,16 +3,17 @@ def find_twenty(numbers, sum_result=2020):
     """
     Finds two numbers from a list that sum to sum_result and returns them
 
-    >>> find_twenty([2020, 10, 0])
-    (2020, 0)
+    >>> find_twenty([2005, 10, 0, 8, 7])
+    (2005, 8, 7)
     """
     for n, first in enumerate(numbers):
         for j, second in enumerate(numbers):
-            if n == j:
-                continue
-            total = first + second
-            if total == sum_result:
-                return first, second
+            for i, third in enumerate(numbers):
+                if n == j and j == i:
+                    continue
+                total = first + second + third
+                if total == sum_result:
+                    return first, second, third
 
 def main():
     with open('numbers.txt', 'r') as out:
@@ -20,8 +21,8 @@ def main():
         numbers = [int(i) for i in numbers.split()]
 
     result = find_twenty(numbers)
-    prod = lambda x, y : x * y
-    print(prod(result[0], result[1]))
+    prod = lambda x, y, z : x * y * z
+    print(prod(result[0], result[1], result[2]))
 
 if __name__ == "__main__":
     import doctest
