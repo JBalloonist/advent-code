9e4acfcb32e6e27b1842a7a342c1db619164c758
Rob Ward <wardrobert@gmail.com>
2020-12-04 22:44:34 -0500

    part 1 complete

--- day1/report.py
+++ day1/report.py
@@ -1,17 +1,29 @@
 
-
-with open('numbers.txt', 'r') as out:
-    numbers = out.read()
-
 def find_twenty(numbers, sum_result=2020):
+    """
+    Finds two numbers from a list that sum to sum_result and returns them
+
+    >>> find_twenty([2020, 10, 0])
+    (2020, 0)
+    """
     for n, first in enumerate(numbers):
         for j, second in enumerate(numbers):
             if n == j:
                 continue
             total = first + second
             if total == sum_result:
-                print(total)
-                print(f'first: {first} second: {second}')
                 return first, second
-                
-find_twenty(numbers)
+
+def main():
+    with open('numbers.txt', 'r') as out:
+        numbers = out.read()
+        numbers = [int(i) for i in numbers.split()]
+
+    result = find_twenty(numbers)
+    prod = lambda x, y : x * y
+    print(prod(result[0], result[1]))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    main()
