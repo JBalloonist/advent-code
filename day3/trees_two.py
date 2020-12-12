bf5f8bfe163850462592330335e49035e3fa3f4d
Rob Ward <wardrobert@gmail.com>
2020-12-12 11:35:27 -0500

    answered 73 but not right

--- day3/trees_two.py
+++ day3/trees_two.py
@@ -11,11 +11,15 @@ def main(lines):
     for l_num, line in enumerate(lines):
         for pos_num, location in enumerate(line):
             if pos_num == pos_start and l_num == line_start:
+                print(f'Line Number: {l_num}; Line: {line};\
+                Loc: {location}; Pos_num: {pos_start}')
                 trees.append(location)
                 line_start += 1
                 pos_start += 3
+            if (len(line) // 3) % 10 == 0:
+                pos_start = 3
 
-    return Counter(trees) 
+    return Counter(trees)
 
 
 if __name__ == "__main__":
